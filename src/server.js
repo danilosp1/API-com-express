6265c6a6847e768df02c26db38fc82965940f3d5
Danilo da Silva Pinto <danilo.sp03@gmail.com>
2021-10-10 13:37:19 -0300

    'Finalizado'

--- src/server.js
+++ src/server.js
@@ -2,13 +2,43 @@ const port = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const db = require('./db')
 
 // .use -> universal middleware
 
-app.get('/produtos', (req, res, next) => {
-    res.send({ name: 'Notebook', price: 1203.99 }) // Object -> JSON
+// All server requests will go through this middleware
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/products', (req, res, next) => {
+    res.send(db.getProducts()) // Object -> JSON
 })
 
+app.get('/products/:id', (req, res, next) => {
+    res.send(db.getProduct(req.params.id))
+})
+
+app.post('/products', (req, res, next) => {
+    const product = db.saveProduct({
+        name: req.body.name,
+        price: req.body.price
+    })
+    res.send(product) // JSON
+})
+
+app.put('/products/:id', (req, res, next) => {
+    const product = db.saveProduct({
+        id: req.params.id,
+        name: req.body.name,
+        price: req.body.price
+    })
+    res.send(product)
+})
+
+app.delete('/products/:id', (req, res, next) => {
+    const product = db.delProduct(req.params.id)
+    res.send(product)
+})
 
 app.listen(port, () => {
     console.log(`Server open - port: ${port}`)
